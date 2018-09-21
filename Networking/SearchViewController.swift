0d8248d94cb00165422594b780086907c8074575
jrichardson <jrichardson@freshbooks.com>
2018-09-21 09:01:26 -0400

    updates to SearchViewController

--- Networking/SearchViewController.swift
+++ Networking/SearchViewController.swift
@@ -8,11 +8,25 @@
 
 import UIKit
 
-class SearchViewController: UITableViewController {
+class SearchViewController: UIViewController {
+    
+    // MARK: Outlets
+    
+    @IBOutlet weak var searchBar: UISearchBar!
+    @IBOutlet weak var tableView: UITableView!
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        placeTableViewBelowSearchBar()
     }
+    
+    // MARK: Table View Customizations
+    
+    func placeTableViewBelowSearchBar() {
+        tableView.contentInset = UIEdgeInsets(top: 64, left: 0, bottom: 0, right: 0)
+    }
+    
+    // MARK: Networking
     
     func freshbooksURL(searchText: String) -> URL {
         let urlString = String(format: "https://itunes.apple.com/search?term=%@", searchText)
@@ -22,3 +36,12 @@ class SearchViewController: UITableViewController {
 
 }
 
+extension SearchViewController: UISearchBarDelegate {
+    func searchBarSearchButtonClicked(_ searchBar: UISearchBar) {
+        if let searchText = searchBar.text {
+            print("The search bar text is: '\(searchText)'")
+        } else {
+            print("No text in search bar")
+        }
+    }
+}
